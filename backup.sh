5392a0cdc85b0c4fc3ead281ad18581853449a39
888ynk888 <michaljasinki028@gmail.com>
2020-02-26 22:17:46 +0100

    1st

--- backup.sh
+++ backup.sh
@@ -1,1 +1,10 @@
-......
+#!/bin/bash
+
+PASSWORD=123
+BACKUPNAME=backup.zip
+BACKUPDSTLOC=/home/mj/Files
+IPDST=root@10.58.5.26
+BACKUPDST=/home/mj/backup/
+zip -r --password $PASSWORD $BACKUPNAME $BACKUPDSTLOC
+
+rsync -avze ssh $BACKUPNAME $IPDST:$BACKUPDST
